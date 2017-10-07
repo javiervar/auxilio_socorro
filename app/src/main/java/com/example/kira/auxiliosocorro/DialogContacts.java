99835ceb9e802e341fe85aa6f22027cd5898b76c
Manuel Torres <josem.torress95@gmail.com>
2017-10-07 06:41:44 -0700

    obtencion de contactos lista, espero

--- app/src/main/java/com/example/kira/auxiliosocorro/DialogContacts.java
+++
@@ -1,8 +0,0 @@
-package com.example.kira.auxiliosocorro;
-
-/**
- * Created by jose_ on 07/10/2017.
- */
-
-public class DialogContacts {
-}
--- app/src/main/java/com/example/kira/auxiliosocorro/FragmentContactos.java
+++ app/src/main/java/com/example/kira/auxiliosocorro/FragmentContactos.java
@@ -1,8 +1,31 @@
 package com.example.kira.auxiliosocorro;
 
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
 /**
  * Created by jose_ on 07/10/2017.
  */
 
-public class FragmentContactos {
+public class FragmentContactos extends AppCompatActivity{
+    private SimpleCursorAdapter adapter;
+
+@Override
+    protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+        setupCursorAdapter();
+        ListView lvContacts =(ListView)findViewById(R.id.dialoglist);
+        lvContacts.setAdapter(adapter);
+    }
+private void setupCursorAdapter(){
+    String[] uiBindFrom = { ContactsContract.Contacts.DISPLAY_NAME, ContactsContract.Contacts.HAS_PHONE_NUMBER};
+    int[] uiBindTo={R.id.list_item, };
+    adapter = new SimpleCursorAdapter(
+            this, R.layout.dialog_contactos,
+            null, uiBindFrom, uiBindTo,
+            0);
+}
 }

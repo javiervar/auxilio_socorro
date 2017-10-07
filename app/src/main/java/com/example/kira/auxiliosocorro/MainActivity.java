4d3eef1140d04ca83a4f5af52a6f91b942674a60
Luis Pastor <jolupara@gmail.com>
2017-10-06 19:02:28 -0700

    creando fragment inicio, primer pantalla

--- app/src/main/java/com/example/kira/auxiliosocorro/MainActivity.java
+++ app/src/main/java/com/example/kira/auxiliosocorro/MainActivity.java
@@ -11,7 +11,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    
+    //agregando el menu del inicio
+    //@Override
+
+
+
 
 
 
